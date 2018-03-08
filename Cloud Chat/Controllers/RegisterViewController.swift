6adb12f31ab9c3fadeedeb989edfb640465cda83
C. Austin Adams <caustinwadams@gmail.com>
2018-03-07 21:05:16 -0500

    Error Labels For Register Page
    Added error labels for registering a user (user exists, bad username format, password not long enough, etc.)

--- Cloud Chat/Controllers/RegisterViewController.swift
+++ Cloud Chat/Controllers/RegisterViewController.swift
@@ -19,10 +19,12 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var registerButton: UIButton!
     
+    @IBOutlet weak var errorLabel: UILabel!
     //MARK: - On Load Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         registerButton.layer.cornerRadius = registerButton.frame.height / 2
+        errorLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +34,7 @@ class RegisterViewController: UIViewController {
 
     //MARK: - Button Methods
     @IBAction func registerPressed(_ sender: AnyObject) {
-        
+        errorLabel.text = ""
         SVProgressHUD.show()
         
         //TODO: Set up a new user on our Firbase database
@@ -41,7 +43,22 @@ class RegisterViewController: UIViewController {
             (user, error) in
                                 
             if error != nil {
-                print(error!)
+                let curError = error!.localizedDescription
+                let userExists =
+                    "The email address is already in use by another account."
+                let badUserFormat = "The email address is badly formatted."
+                let passwordError = "The password must be 6 characters long or more."
+                if curError == userExists ||
+                    curError == badUserFormat {
+                    self.errorLabel.text =
+                        curError == userExists ?
+                        "USER ALREADY EXISTS" :
+                        "USERNAME CANNOT CONTAIN (. , @)"
+                    self.emailTextfield.shake()
+                } else if curError == passwordError {
+                    self.errorLabel.text = "PASSWORD MUST BE AT LEAST 6 CHARACTERS"
+                    self.passwordTextfield.shake()
+                }
             } else {
                 print("Registration Successful!")
                 self.emailTextfield.text! = ""
